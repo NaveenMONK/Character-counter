447d0c2dfee1e213bd00b83b85455f589b68261e
NaveenMONK <naveenbalaji786@gmail.com>
2021-12-07 20:54:51 +0530

    Name changes

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const input = document.querySelector(".text");
 const result = document.querySelector(".result");
 
 input.addEventListener('keyup',() => {
-  result.innerHTML = `${input.value.length} Words`;
+  result.innerHTML = `${input.value.length} Characters`;
 })
 
