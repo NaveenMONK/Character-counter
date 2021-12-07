fbeefbe5e0ae28abcfd11959a9d50043d9062c84
NaveenMONK <naveenbalaji786@gmail.com>
2021-12-07 20:45:35 +0530

    Final version

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.querySelector(".text");
+const result = document.querySelector(".result");
+
+input.addEventListener('keyup',() => {
+  result.innerHTML = `${input.value.length} Words`;
+})
+
